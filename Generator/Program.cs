f7db34a402298ed2b899ddb75072731dd4869e6a
Chomp <jonreid3@gmail.com>
2021-08-13 16:25:19 +0100

    dont pass working path to BotParser - not used copy changes

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -6,18 +6,19 @@ namespace Generator
     {
         static void Main(string[] args)
         {
+            //TODO: pass these into functions to act as whitelist
             string[] botTypes = { "assault", "marksman", "pmcbot", "bossbully", "bosskilla" };
 
             // Read raw bot dumps from live and turn into c# objects
             var workingPath = Directory.GetCurrentDirectory();
             var dumpPath = $"{workingPath}//dumps";
-            var botParser = new BotParser(workingPath, dumpPath, botTypes);
+            var botParser = new BotParser(dumpPath, botTypes);
             var parsedBots = botParser.Parse();
 
             if (parsedBots.Count == 0)
             {
                 Helpers.LoggingHelpers.LogToConsole("no bots found, unable to continue");
-                Helpers.LoggingHelpers.LogToConsole("Check your dumps are in 'Generator\\bin\\Debug\\netcoreapp3.1\\dumps' and start with 'resp.' NOT 'req'");
+                Helpers.LoggingHelpers.LogToConsole("Check your dumps are in 'Generator\\bin\\Debug\\netcoreapp3.1\\dumps' and start with 'resp.' NOT 'req.'");
                 return;
             }
 
