98e3cf15cd94940910b3355fbf1124aed9e6cf67
Chomp <jonreid3@gmail.com>
2021-08-13 16:23:16 +0100

    comments

--- Generator/StringToolsExtensions.cs
+++ Generator/StringToolsExtensions.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Generator
 {
     public static class StringToolsExtensions
     {
+        /// <summary>
+        /// Add a string to a list only if it doesnt already exist
+        /// </summary>
         public static void AddUnique(this IList<string> self, string item)
         {
             if (!self.Contains(item))
